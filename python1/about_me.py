02d512f1676b60203c02dc0f5dcb9438d4a822a3
Programming for Biology <pfb@info03.cshl.edu>
2025-10-14 11:35:07 -0400

    added more facts

---
+++ python1/about_me.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+import sys
+my_name = "Julie"
+fav_color = "Orange"
+fav_actv = "hiking"
+fav_anm = "kodiak bear (Ursus arctos middendorffi)"
+print(f"My name: {my_name}")
+print (f"My favorite color: {fav_color}")
+print(f"My favorite activity: {fav_actv}")
+print(f"My favorite animal: {fav_anm}")
+
+fact1 = sys.argv[1]
+fact2 = sys.argv[2]
+fact3 = sys.argv[3]
+fact4 = sys.argv[4]
+print(fact1, fact2, fact3, fact4) 
