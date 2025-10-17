a45665c0250d524582702c2de878676ee85d4b3c
Programming for Biology <pfb@info03.cshl.edu>
2025-10-17 16:36:51 -0400

    updated for problem4

--- python7/fasta_header.py
+++ python7/fasta_header.py
@@ -3,8 +3,13 @@ import re
 
 #problem3
 with open("Python_07.fasta", "r") as fasta:
+	info = {}
 	for line in fasta:
-		for head_line in re.findall(">+", line):
-			print(line)
-		
+		for head_line in re.finditer(r"^>+(\w*)\|\S+\s(.*)", line):
+			id = head_line.group(1)
+			desc = head_line.group(2)
+			print(f'id:{id}')
+			print(f'desc:{desc}')
+				
+#problem4		
 	
