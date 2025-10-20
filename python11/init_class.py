643595f35e6a44bbb68a1bda2f32eb5fb8e9d45b
Programming for Biology <pfb@info03.cshl.edu>
2025-10-20 12:12:06 -0400

    completed problem set 11

--- python11/init_class.py
+++ python11/init_class.py
@@ -5,9 +5,32 @@ class dna_seq_info(object):
        self.gene_name = gene_name
        self.organism = organism
 
+    #sequence length method:
+    def seq_length(self):
+        seq_l = len(self.sequence)
+        return seq_l
+    
+    #nucleotide count + GC content methods:
+    def nt_count(self):
+        a_count = self.sequence.count('A')
+        t_count = self.sequence.count('T')
+        g_count = self.sequence.count('G')
+        c_count = self.sequence.count('C')
+        GC_content = (g_count + c_count)/len(self.sequence)
+        return a_count, t_count, g_count, c_count
+        
+    #FASTA formatter method
+    def fasta_formatter(self):
+            fasta_header = "> seq#" + "\n"
+            for line in self.sequence:
+                 seq_fasta = self.sequence[0] + fasta_header + self.sequence
+            return seq_fasta
+                 
+
 #create objects
 dna_object_1 = dna_seq_info('GATTACA', 'ABCD', 'Homo Sapiens')
 dna_object_2 = dna_seq_info('TAAGACGC', 'TNAA', 'Fusobacterium nucleatum')
 
 for i in [dna_object_1, dna_object_2]:
-    print('name:', i.gene_name, 'sequence:' , i.sequence)
+    print('name:', i.gene_name, 'sequence:' , i.sequence, 'sequence_length:', 
+    i.seq_length(), 'nt count:', i.nt_count(), 'fasta:', i.fasta_formatter())
